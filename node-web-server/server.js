ca4b8f1aaef45a4a7e7dcecb70004388c94566e4
Shubhanseervi <shubhamseervi@gmail.com>
2017-06-09 12:52:48 +0530

    added partials using handbars

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,15 +4,21 @@ const hbs = require('hbs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
-
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('ScreamIt', (text) => {
+  return text.toUpperCase()
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
+    pageHeader: 'this the header',
     pageTitle: 'Home Page',
-    pageHeading: 'Home Page',
+    pageHeading: 'Home Pageddjdjdj',
     currentYear: new Date().getFullYear()
   });
 });
