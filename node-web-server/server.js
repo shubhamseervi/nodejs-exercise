584ffbc2ca97924f3ce23e46ba9b7b9d0be89370
Shubhanseervi <shubhamseervi@gmail.com>
2017-06-09 14:59:17 +0530

    express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('Server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to log file.');
+    }
+  });
+
+next();
+});
+
+app.use((req, res, next) => {
+  //res.render('maintenance.hbs');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
@@ -36,6 +59,6 @@ app.get('/about', (req, res) => {
 //   });
 // })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up on port 3000');
 });
