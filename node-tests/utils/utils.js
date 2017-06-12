caf6db69398373dd26b4e5cf3e5d3a9d1d597073
Shubhanseervi <shubhamseervi@gmail.com>
2017-06-12 11:43:20 +0530

    Testing code with mocha

---
+++ node-tests/utils/utils.js
@@ -0,0 +1,12 @@
+module.exports.add = (a, b) => a + b;
+
+module.exports.square = (x) => x * x;
+
+module.exports.setName = (user, fullname) => {
+  var names = fullname.split(' ');
+
+  user.firstName = names[0];
+  user.lastName  =  names[1];
+
+  return user;
+ };
---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,31 @@
+const expect = require('expect');
+
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  expect(res).toBe(44).toBeA('number');
+
+  // if(res !== 44){
+  //   throw new Error(`Expected 44 but got ${res}`)
+  // }
+});
+
+
+it('should squre a number', () => {
+
+  it('should set first name and lastname', () => {
+    var user = {location: 'jpdhpur', age: 22};
+    var res = utils.setName(user, 'shubham seervi');
+
+    expect(res).toInclude({
+      firstName: 'shubham',
+      lastname: 'seervi'
+    });
+  })
+
+  // var res = utils.square(2);
+  //
+  // expect({name: 'shubham'}).toEqual({name: 'shubham'});
+});
